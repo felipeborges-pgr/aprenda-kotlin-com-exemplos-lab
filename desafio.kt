01044d716ab55146048317c043865719b95818e6
Felipe Borges <55149396+felipeborges-pgr@users.noreply.github.com>
2023-05-22 14:42:08 -0300

    Complemento e ajuste das funções

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,38 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Usuario(var nome: String, var email: String, var senha: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val nivel: Nivel, var duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg usuarios: Usuario) {
+        inscritos.addAll(usuarios)
+        // TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    val conteudo01 = listOf (
+        	ConteudoEducacional("Java OO", Nivel.AVANCADO, 240), 
+        	ConteudoEducacional("Java Exceptions", Nivel.INTERMEDIARIO, 180), 
+        	ConteudoEducacional("Java Classes", Nivel.BASICO, 120)
+    	)
+    
+    val formacao01 = Formacao("Java Developer", Nivel.AVANCADO, conteudo01)
+    
+    val usuario01 = Usuario("Usuario01", "usuario01@email.com", "123456")
+    val usuario02 = Usuario("Usuario02", "usuario02@email.com", "12345678")
+  
+    formacao01.matricular(usuario01, usuario02)
+    
+    println(formacao01)
+    println(formacao01.inscritos)
+//     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+//     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
